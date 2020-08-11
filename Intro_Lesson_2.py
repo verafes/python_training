9655a9caad2e4689549038ba3c96c524cb8577d8
verafes@gmail.com <verafes@gmail.com>
2020-08-11 15:37:47 -0700

    Create Intro_Lesson_2.py
    Learning Variables and Mathematical equations

---
+++ Intro_Lesson_2.py
@@ -0,0 +1,35 @@
+# Intro Lessons #2
+
+print("Hello world!") 
+print("My name is Vera")
+
+# Variables and mathematical equations
+
+a = 5 # a variable is created 
+
+print(a) # should return 5 
+a = 7
+print(a) # returns new value = 7
+a = a + 1
+print(a) # 7 + 1 = 8
+a +=1
+print(a) # 8 + 1 = 9
+a -=2 
+print(a) # 9 - 2 = 7
+a *= 3 
+print(a) # 7 * 3 = 21
+a /= 2 
+print(a) # 21 / 2 = 10.5
+a = a // 2 
+print(a) # returns 5
+
+a = 3
+remainder = a % 2 #remainder of division 
+print(remainder) # should return 1 
+
+print(remainder == 0) #check if remainder is zero => should return False/True 
+
+# False because reminder = 1 and does not equal zero
+
+
+https://repl.it/@verafes/Intro-Lesson-2
