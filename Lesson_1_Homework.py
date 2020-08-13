ef39b228fc89ad5d45f75cc05ea31957fd80c010
verafes@gmail.com <verafes@gmail.com>
2020-08-12 23:18:56 -0700

    Lesson #1. Homework
    Practicing

---
+++ Lesson_1_Homework.py
@@ -0,0 +1,93 @@
+# Lesson #1. Home work
+
+# Task #1. Print a poem
+print("How the sunshine gladdens the human heart—")
+print("How the sound of the falling rain")
+print("Will cause the tender tears to start,")
+print("And free the soul from pain.")
+print("J. W. Hammond, 'Oprimist'")
+
+# Task #2. 
+# city_name1 = "Roseville"
+# city_name2 = "Auburn" 
+# distance = 48
+# elapsed_time = 12 
+# print(city_name2, "is located in", distance, "km from", city_name1 + ".") 
+# print("It took a tourist", elapsed_time, "hours to get to", city_name2 + "." )
+# print("The tourist travelled from", city_name1, "to", city_name2, "with the speed", distance//elapsed_time , "km per hour.")
+
+# Task #3a. 
+# name = "Vera"
+# print(f"{name}!")
+
+# # Task #3b
+# price_of_toy = 11
+# count = 3
+# total_price = price_of_toy * count
+# print(f"The pice for 1 toy is {price_of_toy} dollars. Costs for {count} toys equal {total_price} dollars.") 
+
+# Task #4a. 
+num1 = 20
+num2 = 3
+div = num1 // num2
+print(div)
+
+# Task #4b. 
+num2 = 3
+power = num2 ** 5
+print(power)
+
+# Task #4c. 
+number = 34
+number2 = 3
+inter = number // number2
+print(inter)
+rem = number % number2
+print(rem)
+
+print(f"{number} // {number2} = {inter}")
+print(f"{number} % {number2} = {rem}")
+
+# Task #5a. A customer had n dollars in cash. After buying some goods, he has in x times less money. How many money does he have now?
+cash = 55
+x = 3
+rem1 = cash / x # reminder of 3 times less money
+print(rem1)
+div1 = round(cash / x, 2) # round off using function 
+print(div1)
+div2 = round(rem1, 2) # rounds off the difined string 
+print(div2)
+print(f"{cash} / {x} = {div1}")
+rem2 = cash // x
+print("A customer had", cash, "dollars. After buying some goods, he has in", x, "times less money.")
+print("Now he has", rem2, "dollars in his pocket.") 
+
+# Task #5b. Set the side value of a square and calculate its area. 
+side = 10
+print(side)
+print(f"Each side of the square is equal {side}.")
+print("Square area is", side ** 2)
+
+# Task #5c. Set time in format hours:minutes:seconds and calculate total time in seconds. 
+hour = 2
+minutes = 35
+seconds = 12
+print(f"{hour} hours and {minutes} minutes and {seconds} seconds are  equal {hour*60**2 + minutes*60 + seconds} seconds passed after midnight.")
+
+# Task #5c
+lilies_square = 120
+day = 24
+print("The water lilies area in a lake is", lilies_square, ".")
+print(f"The water lilies double every {day} hours.")
+print(f"So in a day, the area with water lilies in the lake will be {lilies_square * 2}.")
+
+# Task #5d
+staff = 23
+print("There were", staff, "workers in office befire the half of the staff took their vacations. Now there are", staff // 2, "clerks.")
+
+# Task #5e
+Chess_squares = 64
+print("On the 64th square of the chessboard alone, there would be", 2 ** (Chess_squares - 1), "grains.")
+
+https://repl.it/@verafes/Homework1
+ 
