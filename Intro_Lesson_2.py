e1eb4f00808c20902b6efb801a682f40b80b1a92
verafes@gmail.com <verafes@gmail.com>
2020-08-31 17:09:25 -0700

    Intro Lesson #2
    Updating

--- Intro_Lesson_2.py
+++ Intro_Lesson_2.py
@@ -31,5 +31,38 @@ print(remainder == 0) #check if remainder is zero => should return False/True
 
 # False because reminder = 1 and does not equal zero
 
+# Formatting strings
+first_name = "Vera"
+print(first_name)
+last_name = "Fesianava"
+print(first_name, last_name)
+print(first_name)
+print(last_name)
+print(first_name + last_name) #consignation
+full_name = first_name + " " + last_name 
+print(full_name)
+age = 10
+print(full_name, age)
+print(full_name + ", " +str(age))
+
+print(f"{full_name}, {age} years old")
+print(f"{first_name} {last_name}, --> {age}")
+
+price_of_toy = 27
+count = 17
+total_price = price_of_toy * count
+print(f"Price of {count} toys equal {total_price}")
+
+print(f"Price of {count} toys equal {price_of_toy * count}")
+
+# Classes
+number = 10 
+num = 3.2 
+name = "Bob" 
+is_student = True 
+print(type(number)) # class 'int'
+print(type(name)) # class 'str'
+print(type(is_student)) # class 'bool' (boolean)
+print(type(num)) # class 'float'
 
 
